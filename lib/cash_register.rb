295f94a0dc93ac38756e9a3ff82024be02708def
Olena Matokhina <olena@matokhina.com>
2019-12-10 19:37:03 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
-
+require 'pry'
+class CashRegister
+  
+  attr_accessor :discount, :total
+  def initialize (employee_discount = 0)
+    @total = 0.0
+    @discount = employee_discount
+  end
+  
+  def add_item (item_name, price, qty = 1)
+    @total += price * qty
+  end
+  
+  def apply_discount 
+    binding.pry
+    @total *= (100 - discount) / 100
+    
+  end
+  
+end
